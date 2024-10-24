94729e10c682ffb8d3af6bb5f402d60820ce2330
TasnimKulle <tasniim.tak@gmail.com>
2024-10-24 09:54:09 +0300

    smooth scrolling

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const toggleButton=document.querySelector('.toggle-button');
 const navbar=document.querySelector('.nav-bar')
+const navLinks = document.querySelectorAll('.navbar-link a');
 toggleButton.addEventListener('click',function(){
     navbar.classList.toggle('active')
     console.log("clicked ......")
 })
 
-  
+document.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        const sectionId = section.getAttribute('id');
+
+        if (scrollY >= sectionTop - sectionHeight / 3) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if (link.getAttribute('href') === `#${sectionId}`) {
+                    link.classList.add('active');
+                }
+            });
+        }
+    });
+});
